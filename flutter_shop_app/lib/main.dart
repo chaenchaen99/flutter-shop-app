9660b18a41a9afbf90343800230bc94fd1ee29ec
chaeyeon <chaenjung.99@gmail.com>
2024-09-30 14:51:17 +0900

    firebase authentication 연동

--- flutter_shop_app/lib/main.dart
+++ flutter_shop_app/lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_shop_app/common/utils/app_styles.dart';
+import 'package:flutter_shop_app/firebase_options.dart';
 import 'package:flutter_shop_app/pages/sign_in/sign_in.dart';
 import 'package:flutter_shop_app/pages/sign_up/sign_up.dart';
 import 'package:flutter_shop_app/pages/welcome/welcome.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
