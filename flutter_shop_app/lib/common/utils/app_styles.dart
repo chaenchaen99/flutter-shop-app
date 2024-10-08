72fe6f59c0b806432124edfc3e4931ce5d737d88
chaeyeon <chaenjung.99@gmail.com>
2024-10-08 13:32:08 +0900

    feat) home page bottom navigation bar 구현

--- flutter_shop_app/lib/common/utils/app_styles.dart
+++ flutter_shop_app/lib/common/utils/app_styles.dart
@@ -6,6 +6,12 @@ class AppTheme {
   static ThemeData appThemeData = ThemeData(
     brightness: Brightness.light,
     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    scaffoldBackgroundColor: Colors.white,
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: Colors.white,
+      selectedItemColor: AppColors.primaryElement,
+      unselectedItemColor: Colors.grey,
+    ),
     appBarTheme: const AppBarTheme(
         elevation: 0,
         centerTitle: true,
